068a027727cbeaeaa8335c513e7bd49aeb37c7f9
jiangj <jiangjian318@gmail.com>
2018-11-01 10:48:44 +0800

    新增

---
+++ src/main/java/com/jiangj/title/TitleWindowsTestCreateEvent.java
@@ -0,0 +1,66 @@
+package com.jiangj.title;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jiang Jian
+ * @since 2018/10/13
+ */
+public class TitleWindowsTestCreateEvent extends AbstractJavaSamplerClient {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+
+    @Override
+    public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+
+        SampleResult result = new SampleResult();
+        try {
+            result.sampleStart();
+
+            //=============start ========
+            HttpHeaders requestHeaders = new HttpHeaders();
+            List<MediaType> mediaTypes = new ArrayList<>();
+            mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+            requestHeaders.setAccept(mediaTypes);
+            ResponseEntity<String> responseEntity=restTemplate.
+                    getForEntity("http://10.50.8.133:6060/ms/api/v1/title/search?name=四川",String.class);
+
+         if (null != responseEntity && null != responseEntity.getBody()){
+             String resultBody = responseEntity.getBody();
+             result.setResponseData(resultBody,"utf-8");
+             result.setSuccessful(true);
+         }else {
+             result.setResponseData("查询失败","utf-8");
+             result.setSuccessful(false);
+         }
+         //=============end ========
+        }catch (Exception e){
+            result.setSuccessful(false);
+            e.printStackTrace();
+        }finally {
+            result.sampleEnd();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TitleWindowsTestCreateEvent tets = new TitleWindowsTestCreateEvent();
+        Arguments arg = new Arguments();
+        JavaSamplerContext context = new JavaSamplerContext(arg);
+        SampleResult result = tets.runTest(context);
+
+        System.out.println(new String(result.getResponseData()));
+    }
+
+}
